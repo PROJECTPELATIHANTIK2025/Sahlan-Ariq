43e287f1a4b34f82370a818f5668a0d03e3a9a5d
PROJECTPELATIHANTIK2025 <mohalan0808@gmail.com>
2025-10-21 07:37:41 +0700

    Change alert emoji from smiling to grinning

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ const tombolSapa = document.querySelectorAll('.sapaButton');
 tombolSapa.forEach(btn => {
   btn.addEventListener('click', function () {
     const nama = this.parentElement.querySelector('h2').textContent;
-    alert(`Halo, kamu baru saja menyapa ${nama}! 😊`);
+    alert(`Halo, kamu baru saja menyapa ${nama}! 😄`);
   });
 });
