7dd8da3fc9c795b63d1c9ac2284346e34c1ccd48
PROJECTPELATIHANTIK2025 <mohalan0808@gmail.com>
2025-10-21 08:08:28 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-const tombolSapa = document.querySelectorAll(".sapaButton");
-
-tombolSapa.forEach((btn) => {
-  btn.addEventListener("click", function () {
-    const nama = this.parentElement.querySelector("h2").textContent;
-    alert(`Halo, terimakasih sudah berkunjung ${nama}! 😄`);
-  });
-});
+function sapa(nama) {
+  alert(`Halo! Kamu sedang menyapa ${nama}. Terima kasih sudah mampir 😄`);
+}
