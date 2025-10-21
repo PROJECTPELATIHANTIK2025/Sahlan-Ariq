dccaf4437099ec6bbf7ad9751acc31c9a080a584
PROJECTPELATIHANTIK2025 <mohalan0808@gmail.com>
2025-10-21 07:24:21 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-const tombolSapa = document.getElementById('sapaButton');
-tombolSapa.addEventListener('click', function () {
-    alert('Halo!! Terimakasih telah berkunjung.')
+const tombolSapa = document.querySelectorAll('.sapaButton');
+
+tombolSapa.forEach(btn => {
+  btn.addEventListener('click', function () {
+    const nama = this.parentElement.querySelector('h2').textContent;
+    alert(`Halo, kamu baru saja menyapa ${nama}! 😊`);
+  });
 });
