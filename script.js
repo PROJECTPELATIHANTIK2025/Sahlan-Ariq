4ab3a878892939e7775ff431a2812d82dedc04f5
PROJECTPELATIHANTIK2025 <mohalan0808@gmail.com>
2025-10-21 07:56:41 +0700

    Update greeting message in alert

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ const tombolSapa = document.querySelectorAll(".sapaButton");
 tombolSapa.forEach((btn) => {
   btn.addEventListener("click", function () {
     const nama = this.parentElement.querySelector("h2").textContent;
-    alert(`Halo, kamu baru saja menyapa ${nama}! 😄`);
+    alert(`Halo, terimakasih sudah berkunjung ${nama}! 😄`);
   });
 });
